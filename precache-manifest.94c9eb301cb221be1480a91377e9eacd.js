5e186dda5fa4dc768eed04ff9726c862cf5c7e51
khedher-asma <khedherasma90@gmail.com>
2019-06-20 16:40:03 +0100

    Updates

---
+++ precache-manifest.94c9eb301cb221be1480a91377e9eacd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5c2cd32128d93bf984069962d4cec42",
+    "url": "/react-credit-card/index.html"
+  },
+  {
+    "revision": "e525af6c33621b0437af",
+    "url": "/react-credit-card/static/css/main.2be07f7d.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/react-credit-card/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "e525af6c33621b0437af",
+    "url": "/react-credit-card/static/js/main.fb05da70.chunk.js"
+  },
+  {
+    "revision": "2b1e9e110329a4c3b295",
+    "url": "/react-credit-card/static/js/runtime~main.14ed8f70.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-credit-card/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4ba08830bdba694f64404ddd6dc2c54a",
+    "url": "/react-credit-card/static/media/puce.4ba08830.png"
+  }
+]);
